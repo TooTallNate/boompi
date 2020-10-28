ff05f25aa9a1a8127054ee8bc00cffd3d1e94cf4
Nathan Rajlich <n@n8.io>
2020-10-28 02:08:22 -0700

    Add Typescript "paths"

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,4 @@
-import styles from '../styles/index.module.css';
+import styles from '@styles/index.module.css';
 
 export default function Index() {
 	return <main className={styles.main}>Hi from Pi</main>;
