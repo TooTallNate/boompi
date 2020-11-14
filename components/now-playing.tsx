fd3fafa3f998c8399e6a874089647e9703c9bbc9
Nathan Rajlich <n@n8.io>
2020-11-14 02:23:08 -0800

    Sliders

--- components/now-playing.tsx
+++ components/now-playing.tsx
@@ -3,23 +3,32 @@ import styles from '@styles/now-playing.module.css';
 
 // Icons
 import Play from '@components/icons/play';
-import Previous from '@components/icons/previous';
-import Next from '@components/icons/next';
+import Rewind from '@components/icons/rewind';
+import Volume from '@components/icons/volume';
+import FastForward from '@components/icons/fast-forward';
 
 export default function NowPlaying({}) {
+	function onPlay(...args) {
+		console.log('onPlay', args);
+	}
 	return <div className={styles.nowPlaying}>
 		<div className={styles.artist}>Pink Floyd</div>
 		<div className={styles.track}>Comfortably Numb</div>
 		<div className={styles.album}>The Wall</div>
 		<div className={styles.position}>
-			<hr />
-			00'00"
-			<hr />
+			1:36
+			<input type="range" min="0" max="100" />
+			-2:14
 		</div>
 		<div className={styles.controls}>
-			<Previous />
-			<Play />
-			<Next />
+			<Rewind />
+			<Play onClick={onPlay} />
+			<FastForward />
+		</div>
+		<div className={styles.volume}>
+			<Volume />
+			<input type="range" min="0" max="100" />
+			<Volume level={3} />
 		</div>
 	</div>
 }
