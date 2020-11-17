d1ff004bb459b7cff2367cb07a99e74cad41d11f
Nathan Rajlich <n@n8.io>
2020-11-16 22:34:49 -0800

    Implement volume change backend

--- frontend/components/now-playing.tsx
+++ frontend/components/now-playing.tsx
@@ -24,28 +24,62 @@ interface NowPlayingProps {
 	onPositionChange: (value: number) => void;
 }
 
-export default function NowPlaying({artist, track, album, volume, onPlay,onPause, onRewind, onFastForward, onVolumeChange, onPositionChange}: NowPlayingProps) {
-	const onVolume = useCallback((event) => {
-		onVolumeChange(event.currentTarget.value / 100);
-	}, [onVolumeChange]);
-	return <div className={styles.nowPlaying}>
-		<div className={styles.artist}>{artist}</div>
-		<div className={styles.track}>{track}</div>
-		<div className={styles.album}>{album}</div>
-		<div className={styles.position}>
-			1:36
-			<input type="range" min="0" max="100" />
-			-2:14
+export default function NowPlaying({
+	artist,
+	track,
+	album,
+	volume,
+	onPlay,
+	onPause,
+	onRewind,
+	onFastForward,
+	onVolumeChange,
+	onPositionChange,
+}: NowPlayingProps) {
+	const onVolume = useCallback(
+		(event) => {
+			onVolumeChange(event.currentTarget.value / 100);
+		},
+		[onVolumeChange]
+	);
+	const onVolumeMute = useCallback(
+		(event) => {
+			onVolumeChange(0);
+		},
+		[onVolumeChange]
+	);
+	const onVolumeMax = useCallback(
+		(event) => {
+			onVolumeChange(1);
+		},
+		[onVolumeChange]
+	);
+	return (
+		<div className={styles.nowPlaying}>
+			<div className={styles.artist}>{artist}</div>
+			<div className={styles.track}>{track}</div>
+			<div className={styles.album}>{album}</div>
+			<div className={styles.position}>
+				1:36
+				<input type="range" min="0" max="100" />
+				-2:14
+			</div>
+			<div className={styles.controls}>
+				<Rewind onClick={onRewind} />
+				<Play onClick={onPlay} />
+				<FastForward onClick={onFastForward} />
+			</div>
+			<div className={styles.volume}>
+				<Volume onClick={onVolumeMute} />
+				<input
+					type="range"
+					min="0"
+					max="100"
+					onInput={onVolume}
+					value={volume * 100}
+				/>
+				<Volume level={3} onClick={onVolumeMax} />
+			</div>
 		</div>
-		<div className={styles.controls}>
-			<Rewind onClick={onRewind} />
-			<Play onClick={onPlay} />
-			<FastForward onClick={onFastForward} />
-		</div>
-		<div className={styles.volume}>
-			<Volume />
-			<input type="range" min="0" max="100" onInput={onVolume} value={volume * 100} />
-			<Volume level={3} />
-		</div>
-	</div>
+	);
 }
--- frontend/lib/use-backend.ts
+++ frontend/lib/use-backend.ts
@@ -9,28 +9,42 @@ interface UseBackendOptions {
 }
 
 export default function useBackend({ url }: UseBackendOptions) {
-	const [ ws, setWs ] = useState(null);
-	const [ volume, setVolumeState ] = useState(0);
+	const [ws, setWs] = useState(null);
+	const [volume, setVolumeState] = useState(0);
 
-	const setVolume = useCallback((value: number) => {
-		debug('Setting volume: %o', value);
-		ws?.send(JSON.stringify({ volume: value }));
-		setVolumeState(value);
-	}, [ws]);
+	const setVolume = useCallback(
+		(value: number, backend = true) => {
+			debug('Setting volume: %o', value);
+			if (backend) {
+				ws?.send(JSON.stringify({ volume: value }));
+			}
+			setVolumeState(value);
+		},
+		[ws]
+	);
+
+	const onMessage = useCallback((event: MessageEvent) => {
+		const body = JSON.parse(event.data);
+		console.log({ body });
+		if (typeof body.volume === 'number') {
+			setVolume(body.volume, false);
+		}
+	}, []);
 
 	useEffect(() => {
 		debug('Creating WebSocket connection: %o', url);
 		const socket = new ReconnectingWebSocket(url);
+		socket.addEventListener('message', onMessage);
 		setWs(socket);
 		return () => {
 			debug('Closing WebSocket connection');
 			socket.close();
 			setWs(null);
 		};
-	}, [url]);
+	}, [url, onMessage]);
 
 	return {
 		volume,
-		setVolume
+		setVolume,
 	};
 }
