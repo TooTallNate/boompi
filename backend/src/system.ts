67e78c5faba494c7f7b34356fef735c39537740c
Nathan Rajlich <nathan@tootallnate.net>
2020-12-02 08:42:35 +0000

    Add debug to system `getVolume()`

--- backend/src/system.ts
+++ backend/src/system.ts
@@ -1,6 +1,10 @@
-import execa, { ExecaChildProcess } from 'execa';
+import execa from 'execa';
+import createDebug from 'debug';
+
+const debug = createDebug('boompi:backend:system');
 
 export async function setVolume(volume: number): Promise<void> {
-	const percent = Math.round(volume * 100);
-	await execa('pactl', ['set-sink-volume', '@DEFAULT_SINK@', `${percent}%`]);
+	const percent = `${Math.round(volume * 100)}%`;
+	debug('Setting system volume to %o', percent);
+	await execa('pactl', ['set-sink-volume', '@DEFAULT_SINK@', percent]);
 }
