4ecf5c5b19a3d380d9f759c77d550c1e377a64d3
Nathan Rajlich <nathan@tootallnate.net>
2020-12-06 21:06:58 +0000

    Use `amixer` to set system volume

--- backend/src/system.ts
+++ backend/src/system.ts
@@ -6,5 +6,9 @@ const debug = createDebug('boompi:backend:system');
 export async function setVolume(volume: number): Promise<void> {
 	const percent = `${Math.round(volume * 100)}%`;
 	debug('Setting system volume to %o', percent);
-	await execa('pactl', ['set-sink-volume', '@DEFAULT_SINK@', percent]);
+
+	// Stopped working after a system update for some reason :(
+	//await execa('pactl', ['set-sink-volume', '@DEFAULT_SINK@', percent]);
+
+	await execa('amixer', ['cset', 'numid=1', '--', percent]);
 }
