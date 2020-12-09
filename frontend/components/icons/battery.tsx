5c1891f90e4e2df7c7e4b6dfdd8d7193371ec140
Nathan Rajlich <n@n8.io>
2020-12-09 13:34:03 -0800

    Make battery icon more dynamic

--- frontend/components/icons/battery.tsx
+++ frontend/components/icons/battery.tsx
@@ -10,7 +10,8 @@ interface BatteryParams extends React.ComponentPropsWithoutRef<'svg'> {
 
 export default function Battery(_props: BatteryParams) {
 	const { isCharging = false, percentage = 1, ...props } = _props;
-	let fill;
+
+	// Charging
 	let charge;
 	if (isCharging) {
 		charge = (
@@ -20,36 +21,10 @@ export default function Battery(_props: BatteryParams) {
 			/>
 		);
 	}
-	if (percentage >= 0.95) {
-		// Full
-		fill = (
-			<path
-				data-fill="4"
-				d="M74.9 35.6H17.7c-1.4 0-2.6 1.2-2.6 2.6v23.7c0 1.4 1.2 2.6 2.6 2.6h57.2c1.4 0 2.6-1.2 2.6-2.6V38.1c0-1.4-1.1-2.5-2.6-2.5z"
-			/>
-		);
-	} else if (percentage >= 0.75) {
-		fill = (
-			<path
-				data-fill="3"
-				d="M58.8 37.1c-.4-.9-1.3-1.5-2.4-1.5H17.7c-1.4 0-2.6 1.2-2.6 2.6v23.7c0 1.4 1.2 2.6 2.6 2.6H67c1.9 0 3.1-1.9 2.4-3.6L58.8 37.1z"
-			/>
-		);
-	} else if (percentage >= 0.5) {
-		fill = (
-			<path
-				data-fill="2"
-				d="M45.8 37.1c-.4-.9-1.3-1.5-2.4-1.5H17.7c-1.4 0-2.6 1.2-2.6 2.6v23.7c0 1.4 1.2 2.6 2.6 2.6h36.4c1.9 0 3.1-1.9 2.4-3.6L45.8 37.1z"
-			/>
-		);
-	} else if (percentage >= 0.2) {
-		fill = (
-			<path
-				data-fill="1"
-				d="M26.4 37.1c-.4-.9-1.3-1.5-2.4-1.5h-6.3c-1.4 0-2.6 1.2-2.6 2.6v23.7c0 1.4 1.2 2.6 2.6 2.6h17c1.9 0 3.1-1.9 2.4-3.6L26.4 37.1z"
-			/>
-		);
-	}
+
+	// Fill
+	const fillLevel = 57.2 * percentage;
+
 	return (
 		<svg
 			xmlns="http://www.w3.org/2000/svg"
@@ -57,7 +32,10 @@ export default function Battery(_props: BatteryParams) {
 			{...props}
 		>
 			<path d="M93 37.1h-2.8v-.9c0-7.3-6-13.3-13.3-13.3H15.8c-7.3 0-13.3 6-13.3 13.3v27.6c0 7.3 6 13.3 13.3 13.3h61.1c7.3 0 13.3-6 13.3-13.3v-.9H93c2.5 0 4.5-2 4.5-4.5V41.7c0-2.5-2-4.6-4.5-4.6zm-8.7 26.7c0 4.1-3.3 7.4-7.4 7.4H15.8c-4.1 0-7.4-3.3-7.4-7.4V36.2c0-4.1 3.3-7.4 7.4-7.4h61.1c4.1 0 7.4 3.3 7.4 7.4v27.6z" />
-			{fill}
+			<path
+				data-fill="1"
+				d={`M74.9 35.6H17.7c-1.4 0-2.6 1.2-2.6 2.6v23.7c0 1.4 1.2 2.6 2.6 2.6h${fillLevel.toFixed(1)}c1.4 0 2.6-1.2 2.6-2.6V38.1c0-1.4-1.1-2.5-2.6-2.5z`}
+			/>
 			{charge}
 		</svg>
 	);
