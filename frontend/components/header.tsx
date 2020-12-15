37ad80bd6bbe0d9dc306457617795c09211edcac
Nathan Rajlich <n@n8.io>
2020-12-15 01:52:10 -0800

    Better battery charge styling

--- frontend/components/header.tsx
+++ frontend/components/header.tsx
@@ -32,15 +32,31 @@ export default function Header({
 }: HeaderProps) {
 	const { now } = useNow();
 	const isConnected = typeof bluetoothName === 'string';
+
 	let volumeIcon = null;
 	if (typeof volume === 'number') {
 		const volumeLevel = volume === 0 ? 0 : Math.floor(volume * 3) + 1;
 		volumeIcon = <Volume level={volumeLevel} />;
 	}
-	const batteryClasses = [styles.battery];
-	if (battery && battery.percentage < 0.2) {
-		batteryClasses.push(styles.low);
+
+	let batteryIcon = null;
+	if (battery) {
+		const batteryClasses = [styles.battery];
+		if (battery.percentage < 0.2) {
+			batteryClasses.push(styles.low);
+		}
+		let isCharging = battery.current <= -20;
+		if (isCharging) {
+			batteryClasses.push(styles.charging);
+		}
+		batteryIcon = <BatteryIcon
+						className={batteryClasses.join(' ')}
+						percentage={battery.percentage}
+						isCharging={isCharging}
+						onClick={onBatteryClick}
+					/>
 	}
+
 	return (
 		<section className={styles.header}>
 			<div className={styles.left}>
@@ -59,14 +75,7 @@ export default function Header({
 					className={styles.bluetooth}
 					isConnected={isConnected}
 				/>
-				{battery && (
-					<BatteryIcon
-						className={batteryClasses.join(' ')}
-						percentage={battery.percentage}
-						isCharging={battery.current <= -100}
-						onClick={onBatteryClick}
-					/>
-				)}
+				{batteryIcon}
 			</div>
 		</section>
 	);
