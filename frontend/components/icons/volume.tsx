9eecdf344706b52d730a3d0835b69fa59837bd7f
Nathan Rajlich <n@n8.io>
2021-05-15 00:39:31 -0700

    Prettier

--- frontend/components/icons/volume.tsx
+++ frontend/components/icons/volume.tsx
@@ -1,19 +1,28 @@
-import { SpeakerLoudIcon, SpeakerModerateIcon, SpeakerOffIcon, SpeakerQuietIcon } from './radix';
+import {
+	SpeakerLoudIcon,
+	SpeakerModerateIcon,
+	SpeakerOffIcon,
+	SpeakerQuietIcon,
+} from './radix';
 
 interface VolumeParams extends React.ComponentPropsWithoutRef<'svg'> {
 	mute?: boolean;
 	level?: number;
 }
 
-export default function Volume({ mute = false, level = 0, ...props }: VolumeParams) {
+export default function Volume({
+	mute = false,
+	level = 0,
+	...props
+}: VolumeParams) {
 	if (mute) {
-		return <SpeakerOffIcon {...props} />
+		return <SpeakerOffIcon {...props} />;
 	} else if (level === 3) {
-		return <SpeakerLoudIcon {...props} />
+		return <SpeakerLoudIcon {...props} />;
 	} else if (level === 2) {
-		return <SpeakerModerateIcon {...props} />
+		return <SpeakerModerateIcon {...props} />;
 	} else if (level === 1) {
-		return <SpeakerQuietIcon {...props} />
+		return <SpeakerQuietIcon {...props} />;
 	}
-	return <SpeakerOffIcon {...props} />
+	return <SpeakerOffIcon {...props} />;
 }
